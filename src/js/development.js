a0f71f8b913cb6bc33766d4eb288a566fbe517be
City7g <City7gor@gmail.com>
2022-03-03 19:40:17 +0200

    ver. 0.1.3

--- src/js/development.js
+++ src/js/development.js
@@ -26,6 +26,41 @@ const development = () => {
     e.preventDefault()
     document.querySelector('body').classList.toggle('grid')
   })
+
+  // Fixed Grid
+  document.body.style.setProperty("--screen-width", document.body.scrollWidth + "px");
+  window.addEventListener('resize', () => {
+    document.body.style.setProperty("--screen-width", document.body.scrollWidth + "px");
+  })
+
+  // Screen
+  setInterval(() => {
+    document.querySelector('.screen-width span').textContent = window.screen.width
+    document.querySelector('.screen-height span').textContent = window.screen.height
+    document.querySelector('.screen-screen-left span').textContent = window.screenLeft
+    document.querySelector('.screen-screen-top span').textContent = window.screenTop
+    document.querySelector('.screen-screen-y span').textContent = window.screenY
+    document.querySelector('.screen-screen-x span').textContent = window.screenX
+    document.querySelector('.screen-avail-width span').textContent = window.screen.availWidth
+    document.querySelector('.screen-avail-height span').textContent = window.screen.availHeight
+    document.querySelector('.screen-outer-width span').textContent = window.outerWidth
+    document.querySelector('.screen-outer-height span').textContent = window.outerHeight
+    document.querySelector('.screen-inner-width span').textContent = window.innerWidth
+    document.querySelector('.screen-inner-height span').textContent = window.innerHeight
+    document.querySelector('.screen-client-width span').textContent = document.documentElement.clientWidth
+    document.querySelector('.screen-client-height span').textContent = document.documentElement.clientHeight
+    document.querySelector('.screen-scroll-width span').textContent = document.documentElement.scrollWidth
+    document.querySelector('.screen-scroll-height span').textContent = document.documentElement.scrollHeight
+    document.querySelector('.screen-offset-width span').textContent = document.documentElement.offsetWidth
+    document.querySelector('.screen-offset-height span').textContent = document.documentElement.offsetHeight
+  }, 100)
+
+  window.addEventListener('resize', () => {
+    document.querySelectorAll('h4 span').forEach(item => item.style.color = 'blue')
+    setTimeout(() => {
+      document.querySelectorAll('h4 span').forEach(item => item.style.color = '')
+    }, 50);
+  })
 }
 
 export default development
